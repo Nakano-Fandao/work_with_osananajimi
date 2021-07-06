c649eb0709ebd07d093a1c2ea8df9358fdff40be
kazuki-nishimura <71303399+kazuki-nishimura@users.noreply.github.com>
2021-07-07 03:54:02 +0900

    Improve choicechats

--- UI/play_voice.py
+++ UI/play_voice.py
@@ -5,16 +5,20 @@ import random
 from PySide2.QtMultimedia import QSound
 from json_files import JsonFiles
 
+from settings.mood_parameter import MoodParameter
+
 class PlayVoice():
     def __init__(self):
         self.json = JsonFiles()
         print("Loading 幼馴染の声 completed")
 
     # サボりを検知したとき
-    def play_voice(self, case, mood):
+    def play_voice(self, case, parameter):
         """
         smapho, youtube, hentai, common
         """
+        mood = MoodParameter(parameter).mood
+
         common_voices = list(self.json.voice["common"][mood].values())
         voices = list(self.json.voice[case][mood].values())
         voice = random.choice(voices + common_voices)
@@ -31,10 +35,12 @@ class PlayVoice():
         return annoucement
 
     # 作業を始めたとき
-    def play_app_voice(self, when, mood, touched=False):
+    def play_app_voice(self, when, parameter, touched=False):
         """
         app
         """
+        mood = MoodParameter(parameter).mood
+
         if (when == "start") & (mood == "great"):
             mood = "good"
 
@@ -50,14 +56,25 @@ class PlayVoice():
         return app_voice
 
     # 選択肢のある世間話
-    def play_choicechat(self, mood):
+    def play_choicechat_ask(self, parameter):
         """
         choicechats
         """
+        mood = MoodParameter(parameter).mood
+
         choicechats = list(self.json.voice["choicechat"][mood].values())
-        choicechat = random.choice(choicechats)
+        choicechat = random.choice(choicechats)["ask"]
+        choicechat_detail = self.json.choicechat[choicechat]
         self.find_and_play(choicechat)
-        return choicechat
+        return choicechat, choicechat_detail
+
+        # 選択肢のある世間話
+    def play_choicechat_reply(self, reply):
+        """
+        choicechats
+        """
+        self.find_and_play(reply)
+
 
     # 声を再生する関数
     def find_and_play(self, voice):
--- UI/ui_chat_popup.py
+++ UI/ui_chat_popup.py
@@ -17,21 +17,41 @@ class Ui_ChatPopup(object):
     def setupUi(self, ChatPopup):
         if not ChatPopup.objectName():
             ChatPopup.setObjectName(u"ChatPopup")
-        ChatPopup.resize(540, 105)
+        ChatPopup.resize(540, 295)
         ChatPopup.setStyleSheet(u"background-color: #FF616D;")
-        self.textBrowser_1 = QTextBrowser(ChatPopup)
-        self.textBrowser_1.setObjectName(u"textBrowser_1")
-        self.textBrowser_1.setGeometry(QRect(10, 10, 520, 85))
-        self.textBrowser_1.setStyleSheet(u"QTextBrowser {\n"
+        self.horizontalLayout = QHBoxLayout(ChatPopup)
+        self.horizontalLayout.setSpacing(0)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.chatList = QListWidget(ChatPopup)
+        self.chatList.setObjectName(u"chatList")
+        self.chatList.setFocusPolicy(Qt.NoFocus)
+        self.chatList.setStyleSheet(u"QListWidget::item {\n"
+"	margin: 8px;\n"
+"	padding-top: 20px;\n"
+"	padding-bottom: 20px;\n"
+"	margin-right: 8px;\n"
 "	background-color: #FFEAC9;\n"
-"	border-radius: 15px;\n"
+"	border-radius: 0px;\n"
+"	color: #343A40;\n"
+"	width: 520px;\n"
+"	height: 40px;\n"
+"	font: 75 16pt \"\u30e1\u30a4\u30ea\u30aa\";\n"
+"}\n"
+"QListWidget::item:hover {\n"
+"	margin: 10px;\n"
+"	background-color: #FFFFC9;\n"
+"	border-radius: 0px;\n"
 "	color: #343A40;\n"
 "	font: 75 16pt \"\u30e1\u30a4\u30ea\u30aa\";\n"
+"	word-wrap: true;\n"
+"	\n"
 "}")
-        self.pushButton_1 = QPushButton(ChatPopup)
-        self.pushButton_1.setObjectName(u"pushButton_1")
-        self.pushButton_1.setGeometry(QRect(10, 10, 520, 85))
-        self.pushButton_1.setStyleSheet(u"QPushButton{background: transparent;}")
+        self.chatList.setUniformItemSizes(False)
+        self.chatList.setWordWrap(True)
+
+        self.horizontalLayout.addWidget(self.chatList)
+
 
         self.retranslateUi(ChatPopup)
 
@@ -40,12 +60,5 @@ class Ui_ChatPopup(object):
 
     def retranslateUi(self, ChatPopup):
         ChatPopup.setWindowTitle(QCoreApplication.translate("ChatPopup", u"Dialog", None))
-        self.textBrowser_1.setMarkdown("")
-        self.textBrowser_1.setHtml(QCoreApplication.translate("ChatPopup", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:'\u30e1\u30a4\u30ea\u30aa'; font-size:16pt; font-weight:72; font-style:normal;\">\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>", None))
-        self.pushButton_1.setText("")
     # retranslateUi
 
