6cbfe68f272ca016bf90ea18b01c8dde14b2a5f5
kazuki-nishimura <71303399+kazuki-nishimura@users.noreply.github.com>
2021-07-09 04:26:54 +0900

    Add a new path

--- settings/path_setting.py
+++ settings/path_setting.py
@@ -3,7 +3,7 @@ import os, sys
 class PathSetting:
     def __init__(self):
 
-        add_list = ["../detect_modules", "../UI", "../screens"]
+        add_list = ["../detect_modules", "../UI", "../screens", "../settings"]
 
         for dir in add_list:
             sys.path.append(os.path.normpath(os.path.join(os.path.dirname(__file__), dir)))
