fbae3f2e8b1fe6175c01525b86bbd393b4213ce0
kazuki-nishimura <71303399+kazuki-nishimura@users.noreply.github.com>
2021-07-15 11:26:54 +0900

    Update paths

--- settings/path_setting.py
+++ settings/path_setting.py
@@ -3,7 +3,7 @@ import os, sys
 class PathSetting:
     def __init__(self):
 
-        add_list = ["../detect_modules", "../UI", "../screens", "../settings"]
+        add_list = ["../modules", "../modules/detect_modules", "../UI", "../settings", "../screens"]
 
         for dir in add_list:
             sys.path.append(os.path.normpath(os.path.join(os.path.dirname(__file__), dir)))
