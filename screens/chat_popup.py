a5dbddf37c1816810867cc8271517186cdc7815a
kazuki-nishimura <71303399+kazuki-nishimura@users.noreply.github.com>
2021-07-15 22:51:31 +0900

    Set a popup geometry

--- screens/chat_popup.py
+++ screens/chat_popup.py
@@ -1,10 +1,11 @@
 import sys, os
 
-from PySide2.QtCore import *
+from PySide2.QtCore import QRect, QCoreApplication
 from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../UI"))
+
 from ui_chat_popup import Ui_ChatPopup
 
 class ChatPopup(QDialog):
@@ -18,9 +19,19 @@ class ChatPopup(QDialog):
         self.ui.chatList.clicked.connect(self.accept)
 
     def setupChats(self):
+        desktop = QCoreApplication.instance().desktop()
+        screen_counts = desktop.screenCount()
+        screen_width = desktop.width()
+        screen_height = desktop.height()
+        if screen_counts != 1:
+            if screen_width not in set([
+                1920, 1680, 1600, 1440, 1400, 1366, 1360, 1280, 1152, 1024, 832, 800
+            ]):
+                screen_width /= desktop.screenCount()
+
         chat_length = len(self.chats)
         height = 10 + chat_length*95
-        self.setGeometry(QRect(300, 100, 540, height))
+        self.setGeometry(QRect(screen_width-540, 50, 540, height))
         for i in range(chat_length):
             QListWidgetItem(self.ui.chatList)
             chatItem = self.ui.chatList.item(i)
@@ -34,6 +45,12 @@ class ChatPopup(QDialog):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = ChatPopup()
+    chats = [
+        "関係ないかぁ、ぐすん",
+        "あー、ごめんね。まあ来世はうまくいくといいね。",
+        "//昔じゃなくて今を見てよ。じゃなきゃどっか行っちゃうかもよ"
+    ]
+
+    window = ChatPopup(chats)
     window.show()
     sys.exit(app.exec_())
