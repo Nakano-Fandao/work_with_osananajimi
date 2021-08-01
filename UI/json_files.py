002822f95ebe72c6f6f86fafe8879222d13fa929
kazuki-nishimura <71303399+kazuki-nishimura@users.noreply.github.com>
2021-08-01 23:56:49 +0900

    Modify for cp932 error

--- UI/json_files.py
+++ UI/json_files.py
@@ -5,12 +5,12 @@ class JsonFiles:
         # Load json file
         json_path = os.path.join(os.path.dirname(__file__),'json')
 
-        with open(json_path+"/voice.json") as f:
+        with open(json_path+"/voice.json", encoding="utf-8_sig") as f:
             self.voice = json.load(f)
         self.voice = self.voice["voice"]
 
-        with open(json_path+"/correspondence.json") as f:
+        with open(json_path+"/correspondence.json", encoding="utf-8_sig") as f:
             self.correspondence = json.load(f)
 
-        with open(json_path+"/choicechat.json") as f:
+        with open(json_path+"/choicechat.json", encoding="utf-8_sig") as f:
             self.choicechat = json.load(f)
