ef4c3d11436d99a24c064598761b09ba738bccbe
kazuki-nishimura <71303399+kazuki-nishimura@users.noreply.github.com>
2021-08-01 23:30:48 +0900

    Update screens

--- screens/special_splash_screen.py
+++ screens/special_splash_screen.py
@@ -9,62 +9,60 @@ from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 
 ## ==> MAIN WINDOW
-# from index_screen import IndexScreen
+from index_screen import IndexScreen
 
 ## ==> SPLASH SCREEN
-from ui_splash_screen import Ui_SplashScreen
+from ui_special_splash_screen import Ui_SpecialSplashScreen
 
 
 # SPLASH SCREEN
 class SplashScreen(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
-        self.ui = Ui_SplashScreen()
+        self.ui = Ui_SpecialSplashScreen()
         self.ui.setupUi(self)
 
-    #     ## REMOVE TITLE BAR
-    #     self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
-    #     self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        ## REMOVE TITLE BAR
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
 
-    #     self.loading_list = [
-    #         "load_",
-    #         "load_0",
-    #         "load_0_1",
-    #         "load_1",
-    #         "load_2",
-    #         "load_3",
-    #         "load_4",
-    #         "load_5",
-    #         "load_6",
-    #         "load_7",
-    #         "load_8",
-    #         "load_9"
-    #     ]
+        self.loading_list = [
+            "load_",
+            "load_0",
+            "load_0_1",
+            "load_1",
+            "load_2",
+            "load_3",
+            "load_4",
+            "load_5",
+            "load_6",
+            "load_7",
+            "load_8",
+            "load_9"
+        ]
 
-    #     self.counter = 0
-    #     self.timer = QTimer()
-    #     self.timer.timeout.connect(self.load)
-    #     self.timer.start(300)
+        self.counter = 0
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.load)
+        self.timer.start(200)
 
-    #     self.show()
+        self.show()
 
 
-    # def load(self):
+    def load(self):
 
-    #     load_png = self.loading_list[self.counter]
-    #     path = u":image/images/backgrounds/{}.png".format(load_png)
-    #     print(path)
+        load_png = self.loading_list[self.counter]
+        path = u":image/images/backgrounds/{}.png".format(load_png)
 
-    #     self.ui.loadingLabel.setPixmap(QPixmap(path))
+        self.ui.loadingLabel.setPixmap(QPixmap(path))
 
-    #     print(self.counter)
-    #     self.counter += 1
+        self.counter += 1
 
-    #     if self.counter == 12:
-    #         self.timer.stop()
-    #         self.main = IndexScreen()
-    #         self.main.show()
-    #         self.close()
+        if self.counter == 12:
+            self.timer.stop()
+            self.main = IndexScreen()
+            self.main.show()
+            self.close()
 
 
 if __name__ == "__main__":
